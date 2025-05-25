d186e5a2a49ebc3d26c645b4c0d905934969851d
Hanssbr <rayhaniqbalbr@gmail.com>
2025-05-25 09:30:23 +0700

    my rock-paper-scissors game

--- index.js
+++ index.js
@@ -7,3 +7,76 @@ document.getElementById("submit").onclick = function () {
     window.alert(`Halooo, ${answer}`);
   }
 };
+
+let userInput;
+let botInput;
+let botParse;
+
+document.getElementById("submit-btn").onclick = function () {
+  userInput = document.getElementById("choose").value;
+  console.log(`user input ${userInput}`)
+  botInput = Math.random();
+
+  if (botInput < 0.3) {
+    botParse = "kertas";
+  } else if (botInput < 0.6) {
+    botParse = "batu";
+  } else {
+    botParse = "gunting";
+  }
+  console.log(botParse);
+
+  //   Jika user memilih Gunting
+
+  if (userInput == "gunting") {
+    if (botParse == "kertas") {
+      window.alert(
+        `You win!, You choose ${userInput}, and bot choose ${botParse}`
+      );
+    } else if (botParse == "gunting") {
+      window.alert(
+        `You tie!, You choose ${userInput}, and bot choose ${botParse}`
+      );
+    } else if (botParse == "batu") {
+      window.alert(
+        `You lose!, You choose ${userInput}, and bot choose ${botParse}`
+      );
+    }
+  }
+
+  //   Jika user memilih Batu
+
+  if (userInput == "batu") {
+    if (botParse == "kertas") {
+      window.alert(
+        `You lose!, You choose ${userInput}, and bot choose ${botParse}`
+      );
+    } else if (botParse == "gunting") {
+      window.alert(
+        `You win!, You choose ${userInput}, and bot choose ${botParse}`
+      );
+    } else if (botParse == "batu") {
+      window.alert(
+        `You tie!, You choose ${userInput}, and bot choose ${botParse}`
+      );
+    }
+  }
+
+  //   Jika user memilih Kertas
+
+  if (userInput == "kertas") {
+    if (botParse == "kertas") {
+      window.alert(
+        `You tie!, You choose ${userInput}, and bot choose ${botParse}`
+      );
+    } else if (botParse == "gunting") {
+      window.alert(
+        `You lose!, You choose ${userInput}, and bot choose ${botParse}`
+      );
+    } else if (botParse == "batu") {
+      window.alert(
+        `You win!, You choose ${userInput}, and bot choose ${botParse}`
+      );
+    }
+  }
+};
